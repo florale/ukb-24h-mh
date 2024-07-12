0c14ae5654f91f18173184a84ec432a62dc4806b
Flora Le <flora.le@monash.edu>
2024-07-12 13:42:39 +1000

    cleanup

--- ukb-24h-mh-utils.R
+++ ukb-24h-mh-utils.R
@@ -1,3 +1,4 @@
 
 redir <- "/Users/florale/Library/CloudStorage/OneDrive-MonashUniversity/GitHub/projects/ukbiobank/"
 outputdir <- "/Users/florale/Library/CloudStorage/OneDrive-MonashUniversity/PhD/projects/ukbiobank/ukb-24h-mh/output/"
+
