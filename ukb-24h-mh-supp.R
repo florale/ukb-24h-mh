3089dd2279c20a641937da8ec63aadc90a241094
Flora Le <flora.le@monash.edu>
2024-07-30 21:25:50 +1000

    add mhq results to supp + tidyup

--- ukb-24h-mh-supp.R
+++ ukb-24h-mh-supp.R
@@ -139,6 +139,59 @@ m_gad_2023_gam_sub_sleep_q1_obese_insomnia_persistent_99ci <- readRDS(paste0(out
 m_gad_2023_gam_sub_sleep_q2_obese_insomnia_persistent_99ci <- readRDS(paste0(outputdir, "m_gad_2023_gam_sub_sleep_q2_obese_insomnia_persistent_99ci", ".RDS"))
 m_gad_2023_gam_sub_sleep_q3_obese_insomnia_persistent_99ci <- readRDS(paste0(outputdir, "m_gad_2023_gam_sub_sleep_q3_obese_insomnia_persistent_99ci", ".RDS"))
 
+# 2016
+m_phq_2016_gam_sub_sleep_q1 <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2 <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3 <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3", ".RDS"))
+
+m_phq_2016_gam_sub_goodsleep <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_goodsleep", ".RDS"))
+m_phq_2016_gam_sub_insomnia_mild <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_insomnia_mild", ".RDS"))
+m_phq_2016_gam_sub_insomnia_persistent <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_insomnia_persistent", ".RDS"))
+
+m_phq_2016_gam_sub_sleep_q1_goodsleep <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1_goodsleep", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2_goodsleep <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2_goodsleep", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3_goodsleep <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3_goodsleep", ".RDS"))
+
+m_phq_2016_gam_sub_sleep_q1_insomnia_mild <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1_insomnia_mild", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2_insomnia_mild <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2_insomnia_mild", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3_insomnia_mild <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3_insomnia_mild", ".RDS"))
+
+m_phq_2016_gam_sub_sleep_q1_insomnia_moderate <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1_insomnia_moderate", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2_insomnia_moderate <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2_insomnia_moderate", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3_insomnia_moderate <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3_insomnia_moderate", ".RDS"))
+
+m_phq_2016_gam_sub_sleep_q1_insomnia_severe <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1_insomnia_severe", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2_insomnia_severe <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2_insomnia_severe", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3_insomnia_severe <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3_insomnia_severe", ".RDS"))
+
+m_phq_2016_gam_sub_sleep_q1_insomnia_persistent <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q1_insomnia_persistent", ".RDS"))
+m_phq_2016_gam_sub_sleep_q2_insomnia_persistent <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q2_insomnia_persistent", ".RDS"))
+m_phq_2016_gam_sub_sleep_q3_insomnia_persistent <- readRDS(paste0(outputdir, "m_phq_2016_gam_sub_sleep_q3_insomnia_persistent", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1 <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2 <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3 <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1_goodsleep <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1_goodsleep", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2_goodsleep <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2_goodsleep", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3_goodsleep <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3_goodsleep", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1_insomnia_mild <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1_insomnia_mild", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2_insomnia_mild <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2_insomnia_mild", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3_insomnia_mild <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3_insomnia_mild", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1_insomnia_moderate <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1_insomnia_moderate", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2_insomnia_moderate <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2_insomnia_moderate", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3_insomnia_moderate <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3_insomnia_moderate", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1_insomnia_severe <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1_insomnia_severe", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2_insomnia_severe <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2_insomnia_severe", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3_insomnia_severe <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3_insomnia_severe", ".RDS"))
+
+m_gad_2016_gam_sub_sleep_q1_insomnia_persistent <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q1_insomnia_persistent", ".RDS"))
+m_gad_2016_gam_sub_sleep_q2_insomnia_persistent <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q2_insomnia_persistent", ".RDS"))
+m_gad_2016_gam_sub_sleep_q3_insomnia_persistent <- readRDS(paste0(outputdir, "m_gad_2016_gam_sub_sleep_q3_insomnia_persistent", ".RDS"))
+
 # format ------------------------------
 ## dep anx models ----------------------
 sub_models_resp   <- c("m_phq_2023_gam_sub_sleep",
@@ -283,6 +336,149 @@ for(i in seq_along(sub_models_resp)) {
 }
 names(sub_models_dep_anx_insomnia) <- (sub_models_resp)
 
+
+## 2016 models ----------------------
+sub_models_resp   <- c("m_phq_2016_gam_sub_sleep",
+                       "m_gad_2016_gam_sub_sleep"
+)
+sleep_type <- c("_q1", "_q2", "_q3")
+sleep_q_type <- c("_q1", "_q2", "_q3")
+sleep_insomnia_type <- c("_goodsleep", "_insomnia_mild", "_insomnia_persistent")
+
+sub_models_2016 <- list()
+sub_models_sleepg_type <- list()
+
+sub_models_2016_insomnia <- list()
+sub_models_sleep_q_type <- list()
+sub_models_sleep_i_type <- list()
+
+for(i in seq_along(sub_models_resp)) {
+  
+  sub_model_i <- sub_models_resp[[i]]
+  
+  for (j in seq_along(sleep_type)) {
+    
+    sleep_tye_j <- sleep_type[[j]]
+    
+    sleep_period <- if (j == 1) ("Short Sleep Period (Q1)") else if (j == 2) ("Medium Sleep Period (Q2 and Q3)") else ("Long Sleep Period (Q4)")
+    model_tmp <- get(paste0(sub_model_i, sleep_tye_j))
+    model_tmp <- as.data.table(summary(model_tmp, delta = 20, level = "aggregate", digits = "asis"))
+    
+    model_tmp[, SleepPeriod := sleep_period]
+    
+    model_tmp[, From := ifelse(From == "mvpa_comp", "MVPA", From)]
+    model_tmp[, From := ifelse(From == "lpa_comp", "LPA ", From)]
+    model_tmp[, From := ifelse(From == "sb_comp", " SB ", From)]
+    model_tmp[, From := ifelse(From == "sleep_comp", "Sleep", From)]
+    model_tmp[, From := factor(From, ordered = TRUE,
+                               levels = c("Sleep",
+                                          "MVPA",
+                                          "LPA ",
+                                          " SB "))]
+    
+    
+    model_tmp[, To := ifelse(To == "mvpa_comp", "MVPA", To)]
+    model_tmp[, To := ifelse(To == "lpa_comp", "LPA ", To)]
+    model_tmp[, To := ifelse(To == "sb_comp", " SB ", To)]
+    model_tmp[, To := ifelse(To == "sleep_comp", "Sleep", To)]
+    model_tmp[, To := factor(To, ordered = TRUE,
+                             levels = c("Sleep",
+                                        "MVPA",
+                                        "LPA ",
+                                        " SB "))]
+    
+    model_tmp$sig <- between(0, model_tmp$CI_low, model_tmp$CI_high)
+    model_tmp[, Sig := NA]
+    model_tmp[, Sig := ifelse(sig == FALSE & Delta == 20, "*", "")]
+    
+    model_tmp <- model_tmp[, .(Mean = round(Mean, 2), CI_low = round(CI_low, 2), CI_high = round(CI_high, 2), From, To, SleepPeriod, Sig)]
+    sub_models_sleepg_type[[j]] <- model_tmp
+  }
+  sub_models_sleepg_type_i  <- rbindlist(sub_models_sleepg_type)
+  sub_models_sleepg_type_i[, SleepPeriod := factor(SleepPeriod, ordered = TRUE,
+                                                   levels = c("Short Sleep Period (Q1)",
+                                                              "Medium Sleep Period (Q2 and Q3)",
+                                                              "Long Sleep Period (Q4)"))]
+  sub_models_2016[[i]] <- sub_models_sleepg_type_i
+}
+names(sub_models_2016) <- (sub_models_resp)
+
+for(i in seq_along(sub_models_resp)) {
+  sub_model_i <- sub_models_resp[[i]]
+  
+  for (j in seq_along(sleep_q_type)) {
+    sleep_type_j <- sleep_q_type[[j]]
+    sleep_period <- if (j == 1) ("Short Sleep Period (Q1)") else if (j == 2) ("Medium Sleep Period (Q2 and Q3)") else ("Long Sleep Period (Q4)")
+    
+    for (k in seq_along(sleep_insomnia_type)){
+      insomnia_k <- sleep_insomnia_type[[k]]
+      insomnia <- if (k == 1) ("None") else if (k == 2) ("Mild") else ("Moderate-Severe")
+      
+      model_tmp <- get(paste0(sub_model_i, sleep_type_j, insomnia_k))
+      model_tmp <- as.data.table(summary(model_tmp, delta = 20, level = "aggregate", digits = "asis"))
+      
+      model_tmp[, SleepPeriod := sleep_period]
+      model_tmp[, Insomnia := insomnia]
+      
+      model_tmp[, From := ifelse(From == "mvpa_comp", "MVPA", From)]
+      model_tmp[, From := ifelse(From == "lpa_comp", "LPA ", From)]
+      model_tmp[, From := ifelse(From == "sb_comp", " SB ", From)]
+      model_tmp[, From := ifelse(From == "sleep_comp", "Sleep", From)]
+      model_tmp[, From := factor(From, ordered = TRUE,
+                                 levels = c("Sleep",
+                                            "MVPA",
+                                            "LPA ",
+                                            " SB "))]
+      
+      
+      model_tmp[, To := ifelse(To == "mvpa_comp", "MVPA", To)]
+      model_tmp[, To := ifelse(To == "lpa_comp", "LPA ", To)]
+      model_tmp[, To := ifelse(To == "sb_comp", " SB ", To)]
+      model_tmp[, To := ifelse(To == "sleep_comp", "Sleep", To)]
+      model_tmp[, To := factor(To, ordered = TRUE,
+                               levels = c("Sleep",
+                                          "MVPA",
+                                          "LPA ",
+                                          " SB "))]
+      
+      model_tmp[, Insomnia := factor(Insomnia, ordered = TRUE,
+                                     levels = c("None",
+                                                "Mild",
+                                                "Moderate-Severe"))]
+      
+      
+      model_tmp[, Insomnia_label := NA]
+      model_tmp[, Insomnia_label := ifelse(Insomnia == "None", "None", Insomnia_label)]
+      model_tmp[, Insomnia_label := ifelse(Insomnia == "Mild", "Mild", Insomnia_label)]
+      model_tmp[, Insomnia_label := ifelse(Insomnia == "Moderate-Severe", "Moderate-Severe", Insomnia_label)]
+      
+      model_tmp[, Insomnia_label := factor(Insomnia_label, ordered = TRUE,
+                                           levels = c("None",
+                                                      "Mild",
+                                                      "Moderate-Severe"))]
+      
+      
+      model_tmp$sig <- between(0, model_tmp$CI_low, model_tmp$CI_high)
+      model_tmp[, Sig := NA]
+      model_tmp[, Sig := ifelse(sig == FALSE & Delta %in% 20, "*", "")]
+      
+      sub_models_sleep_i_type[[k]] <- model_tmp
+    }
+    sub_models_sleep_i_type_k <- rbindlist(sub_models_sleep_i_type)
+    sub_models_sleep_q_type[[j]] <- sub_models_sleep_i_type_k
+  }
+  sub_models_sleep_q_type_i  <- rbindlist(sub_models_sleep_q_type)
+  sub_models_sleep_q_type_i[, SleepPeriod := factor(SleepPeriod, ordered = TRUE,
+                                                    levels = c("Short Sleep Period (Q1)",
+                                                               "Medium Sleep Period (Q2 and Q3)",
+                                                               "Long Sleep Period (Q4)"))]
+  sub_models_sleep_q_type_i <- sub_models_sleep_q_type_i[, .(Mean = round(Mean, 2), CI_low = round(CI_low, 2), CI_high = round(CI_high, 2), From, To, SleepPeriod, Insomnia, Sig)]
+  
+  sub_models_2016_insomnia[[i]] <- sub_models_sleep_q_type_i
+  
+  
+}
+names(sub_models_2016_insomnia) <- (sub_models_resp)
 
 ## healthy models ----------------------
 sub_models_resp   <- c("m_phq_2023_gam_sub_sleep",
